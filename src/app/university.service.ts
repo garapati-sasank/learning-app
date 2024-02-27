4c894d5e62a3d774da375fc6bbe5369ed938a865
garapati sasank <garapatisasank@gmail.com>
2024-02-26 20:11:25 -0600

    api-Impl

---
+++ src/app/university.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UniversityService {
+
+ http = inject(HttpClient);
+  
+  logicToGetUniversityList(){
+    return this.http.get('https://api.usaspending.gov/api/v2/references/toptier_agencies/');
+  }
+
+  
+}
+
+// checking acc balance 
+// tranfer money
+// updating address 
+
+// check all studends info
+// add new student 
+// updated existing address 
+// delete 
+
+// RETRIEVE - GET
+// CREATE   - POST
+// UPDATE - PUT
+// DELETE - Delete
+
+// GET - http://universities.hipolabs.com/search?country=United+Kingdom
